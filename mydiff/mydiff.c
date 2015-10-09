4ab3177e9403498bb7808ed8d3fa30640385d780
Thomas Muhm <tom.muhm@gmail.com>
2015-10-09 14:57:31 +0200

    add gitignore impl getopt check (testing)

--- mydiff/mydiff.c
+++ mydiff/mydiff.c
@@ -1,14 +1,56 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+const int line_length = 256;
+
+void usage(void);
+
+void error_exit(const char *msg);
 
 int main(int argc, char *argv[]) {
 
+  int c;
+  int aflag = 0;
+  int bflag = 0;
+
+  while((c = getopt(argc, argv, "ab")) != -1) {
+    switch(c) {
+      case 'a':
+        aflag = 1;
+        printf("first par set"); 
+        break;
+      case 'b':
+        printf("second par set");
+        bflag = 1;
+        break;
+      case '?':
+       printf("unkown option");
+       error_exit("test ?");
+       break;
+      default:
+        assert(0);  // imposible
+    }
+  }
+
   FILE *file1, *file2;
-  file1 = fopen(argv[1], "r");
-  file2 = fopen(argv[2], "r");
+  //file1 = fopen(argv[1], "r");
+  //file2 = fopen(argv[2], "r");
 
   
-  
+ 
+  //fclose(file1);
+  //fclose(file2);
 
 
 
+  exit(0);
+}
+
+void usage(void) {
+}
+
+void error_exit(const char *msg) {
+  //(void) printf("mydiff: %s",  msg);
+  exit(-1);
 }
