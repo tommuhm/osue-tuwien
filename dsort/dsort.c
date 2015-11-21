614bfa2685fd673b24d14845edb2f0dd3fc767f6
Thomas Muhm <tom.muhm@gmail.com>
2015-11-21 23:18:51 +0100

    add ugly quick implementation and prepare for cleanup

--- dsort/dsort.c
+++ dsort/dsort.c
@@ -20,7 +20,6 @@
 #include <stdarg.h>
 #include <strings.h>
 
-
 /* === Constants === */
 
 #define LINE_SIZE (1024)
@@ -43,6 +42,16 @@
 /* Name of the program */
 static const char *progname = "dsort"; /* default name */
 
+
+// TODO wohin?
+
+struct command_output {
+	char **content;
+	int line_count;
+};
+
+
+static struct command_output my_output;
 
 /* === Prototypes === */
 
@@ -143,21 +152,22 @@ static void run_command(char *command) {
 				bail_out(errno, "parent: could not open read pipe");			
 			}
 
-			char *output_array[LINE_SIZE];
-			printf("array size %li\n", sizeof(output_array));
-printf("array size %li\n", sizeof(*output_array));			
-int array_size = 0;
+
+			// todo 
+
+			
+
 			while(fgets(buffer, sizeof buffer, read_pipe) != NULL) {
-				if (output_array == NULL) {
-			//		(void) printf("init array with malloc size: %li %li \n", (sizeof *output_array), sizeof output_array);
-			//		output_array = malloc(sizeof *output_array);
-			//		free(output_array);
+				my_output.line_count += 1;
+				if (my_output.line_count == 1) {
+					my_output.content = malloc(my_output.line_count * sizeof (char *));
+				} else {
+					my_output.content = realloc(my_output.content, my_output.line_count * sizeof  (char *));
 				}
-
-
-				(void) printf("parent: pipe read: %s\n", buffer);	
+				my_output.content[my_output.line_count-1] = strdup(buffer);
+				(void) printf("parent: pipe read: %s", buffer);	
 			}
-
+						
 			(void) printf("parent: parent here - wait for child to die?\n"); 
 			while ((pid = wait(&status)) != child_pid ) {
 				if (pid != -1) continue; /* other child */
@@ -173,6 +183,15 @@ int array_size = 0;
 		}
 }
 
+static int cmpstringp(const void *p1, const void *p2)
+{
+   /* The actual arguments to this function are "pointers to
+      pointers to char", but strcmp(3) arguments are "pointers
+      to char", hence the following cast plus dereference */
+
+   return strcmp(* (char * const *) p1, * (char * const *) p2);
+}
+
 int main(int argc, char **argv) {
 
 	if(argc > 0) {
@@ -184,10 +203,84 @@ int main(int argc, char **argv) {
 
 	// TODO richtig parsen alles zwischen hochcomma wird zu einem string !!! is eh automatisch oder?
 
+	bzero(&my_output, sizeof my_output);
+
 	run_command(argv[1]);
 
 	run_command(argv[2]);
 
+	qsort(my_output.content, my_output.line_count, sizeof(char *), cmpstringp);
+
+	printf("printing stuff\n");
+	for (int i = 0; i < my_output.line_count; i++) {
+		printf("%d: %s\n", i, my_output.content[i]);
+	}
+	
+
+
+	// TODO in function auslagern	
+	char *cmd[] = { "uniq", "-d",(char *) 0};
+	
+	// create pipe 
+	int command_pipe[2];
+	if (pipe(command_pipe) != 0) {
+		bail_out(errno, "can't create pipe");	
+	}
+
+	// run child - own functions for child and parent behavior?
+	pid_t pid, child_pid;
+	int status;
+	
+	switch (child_pid = fork()) {
+		case -1: 
+			bail_out(errno, "can't fork");
+			break;
+		case 0:
+			(void) printf("child: child here - exec command\n");
+			(void) printf("child: closing write pipe\n");
+			if (close(command_pipe[1]) != 0) {
+				bail_out(errno, "child: can't close write pipe");			
+			}
+			(void) printf("child: rewire read pipe to stdin\n");
+			(void) fflush(stdout); // TODO error?
+			if (dup2(command_pipe[0], fileno(stdin)) == -1) {
+				bail_out(errno, "child: can't rewire write pipe");			
+			}
+			if (close(command_pipe[0]) != 0) {
+				bail_out(errno, "child: can't close fd after rewire");
+			}							
+			(void) execv("/bin/uniq", cmd);
+			bail_out(errno, "child: can't exec");
+			break;
+		default:
+			(void) printf("parent: closing read pipe\n");
+			if (close(command_pipe[0]) != 0) {
+				bail_out(errno, "parent: can't close read pipe");			
+			}
+			(void) printf("parent: write to pipe\n");
+			
+			char buffer[LINE_SIZE];
+			bzero(&buffer, sizeof buffer);
+		
+			// wait for client to be ready!!!
+			FILE *write_pipe;
+			if ((write_pipe = fdopen(command_pipe[1], "w")) == NULL) {
+				bail_out(errno, "parent: could not open read pipe");			
+			}
+		
+			char *cur_pos = my_output.content;
+			while(fputs(cur_pos, write_pipe) != NULL) {
+				cur_pos +=1;
+			}
+		
+			// parent here
+			break;
+	}
+	
+	// TODO makefile auf tilab funkt net immer? nach scp zb
+
+
+	// Notes/TODOs from Task 
 	// 2 commands von argv laden - done
 
 	// 2 forks fuer kindprozze - done
@@ -202,12 +295,11 @@ int main(int argc, char **argv) {
 	// array und strings von array müssen dynamisch alloziert erden - auf saubere freigabe achten !!
 
 
-	// parent darf erst sterben wenn kinder tot sind
+	// parent darf erst sterben wenn kinder tot sind 
 
-	// array sortieren ? (man 3 qsort)
+	// array sortieren ? (man 3 qsort) - done
 
 	// forken und "uniq -d" starten - sortierte array mit pipes auf stdin von dem kindprozess spielen
-
 
 	// alles muss wieder freigegeben werden!!, auch im fehlerfall!!!
 
